78501e292e00dc2baa51c4006015a60f6cd64e5d
David Bernadi <davidbernadi13@gmail.com>
2022-07-16 14:12:36 +0700

    fix db setup

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -23,5 +23,5 @@ const (
 
 var (
 	ScheduleTime []string = []string{"08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00"}
-	TransStatus  []string = []string{"'Menunggu Pembayaran", "Menunggu Konfirmasi", "Menunggu Kedatangan", "Diproses", "Selesai"}
+	TransStatus  []string = []string{"Menunggu Pembayaran", "Menunggu Konfirmasi", "Menunggu Kedatangan", "Diproses", "Selesai"}
 )
